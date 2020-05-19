2d7f1796b65f03682cd2e78a88afbf7bcf2501e1
Voltra <ludwig.guerin.98@gmx.fr>
2020-05-19 03:20:42 +0200

    Add testing scripts

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const { defaults } = require("jest-config");
+
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testRegex: "\.test\.ts$",
+	moduleFileExtensions: [
+		...defaults.moduleFileExtensions,
+		"ts",
+		"tsx",
+	],
+};
