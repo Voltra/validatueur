fae517e51a8d267eec92dc1e45b85c9043d4466d
Voltra <ludwig.guerin.98@gmx.fr>
2020-05-21 23:15:09 +0200

    Add abstract validator

---
+++ src/validators/AbstractValidator.ts
@@ -0,0 +1,16 @@
+import { Validatueur } from "../api/index"
+import { isNone } from "../api/types";
+import { errorFrom } from "../api/helpers";
+
+export abstract class AbstractValidator<T, U = T> implements Validatueur.Validator<T, U>{
+	public validate(value: T, args: Validatueur.ValidatorArgs): Validatueur.Result<U, Validatueur.Error> {
+		const opt = this.__validate(value);
+		if(isNone(opt))
+			return errorFrom(args, this);
+
+		return opt as U;
+	}
+
+	public abstract get rule(): string;
+	protected abstract __validate(value: T): Validatueur.Optional<U>;
+}
