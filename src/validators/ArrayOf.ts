f4bdf17edae418a1530df7b83ff0038556dbc1b4
Voltra <ludwig.guerin.98@gmx.fr>
2020-05-23 19:34:17 +0200

    Add arrayOf validator

--- src/validators/ArrayOf.ts
+++ src/validators/ArrayOf.ts
@@ -3,15 +3,20 @@ import { Validatueur } from "../api/index";
 import { Schema } from "../api/Schema";
 import { RuleChain } from "../rules";
 
-export class ArrayOf<T = any> extends AbstractValidator<any[], T[]>{
+export class ArrayOf<T = any> extends AbstractValidator<any[], T[]> {
 	public get rule(): string {
 		return "arrayOf";
 	}
 
-	protected async __validate(field: string, values: any[], schema: Schema, rules: RuleChain<T>): Validatueur.Promise<T[]> {
+	protected async __validate(
+		field: string,
+		values: any[],
+		schema: Schema,
+		rules: RuleChain<T>
+	): Validatueur.Promise<T[]> {
 		const ret: T[] = [];
 
-		for(const value of values){
+		for (const value of values) {
 			const newValue = await rules.__validate(field, value, schema);
 			ret.push(newValue);
 		}
