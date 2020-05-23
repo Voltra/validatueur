531d5daaf3a9857a43fbac898676b17db14a3676
Voltra <ludwig.guerin.98@gmx.fr>
2020-05-23 19:58:37 +0200

    Add objectOfShape validator

--- src/validators/ObjectOfShape.ts
+++ src/validators/ObjectOfShape.ts
@@ -4,11 +4,19 @@ import { noneIf } from "../api/types";
 import { Validatueur } from "../api/index";
 import { RuleChain } from "../rules";
 
-export class ObjectOfShape<T = any> extends AbstractValidator<T, Record<string, any>>{
+export class ObjectOfShape<T = any> extends AbstractValidator<
+	T,
+	Record<string, any>
+> {
 	public get rule(): string {
 		return "objectOfShape";
 	}
-	protected async __validate(field: string, value: T, schema: Schema, objectSchema: Schema): Validatueur.Promise<Record<string, any>> {
+	protected async __validate(
+		field: string,
+		value: T,
+		schema: Schema,
+		objectSchema: Schema
+	): Validatueur.Promise<Record<string, any>> {
 		await noneIf(typeof value !== "object", value);
 
 		return objectSchema.validate(value);
