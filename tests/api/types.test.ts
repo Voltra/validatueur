024221a325ebabb5c8ff801ece81fd85f4c12629
Voltra <ludwig.guerin.98@gmx.fr>
2020-06-05 14:53:30 +0200

    Add tests for api types

--- tests/api/types.test.ts
+++ tests/api/types.test.ts
@@ -1,11 +1,11 @@
-import { isNone, none, noneIf } from "@/api/types"
+import { isNone, none, noneIf } from "@/api/types";
 
-describe("isNone", function(){
-	it("is true for none", function(){
+describe("isNone", function () {
+	it("is true for none", function () {
 		expect(isNone(none)).toBeTruthy();
 	});
 
-	it("is false for other values", function(){
+	it("is false for other values", function () {
 		[
 			0,
 			NaN,
@@ -19,22 +19,16 @@ describe("isNone", function(){
 	});
 });
 
-describe("noneIf", function(){
-	it("resolves to the value if the condition is true", function(){
-		[
-			0,
-			42,
-			"str",
-			NaN,
-		].forEach(value => expect(noneIf(true, value)).resolves.toStrictEqual(value));
+describe("noneIf", function () {
+	it("resolves to the value if the condition is true", function () {
+		[0, 42, "str", NaN].forEach(value =>
+			expect(noneIf(true, value)).resolves.toStrictEqual(value)
+		);
 	});
 
-	it("rejects to none if the condition is false", function(){
-		[
-			0,
-			42,
-			"str",
-			NaN,
-		].forEach(value => expect(noneIf(false, value)).rejects.toStrictEqual(none));
+	it("rejects to none if the condition is false", function () {
+		[0, 42, "str", NaN].forEach(value =>
+			expect(noneIf(false, value)).rejects.toStrictEqual(none)
+		);
 	});
 });
