ecd54a4c5cecdeed7daffc91ee3c9c128694b9d4
Voltra <ludwig.guerin.98@gmx.fr>
2020-10-13 08:50:31 +0200

    Update of webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const webpack = require("webpack");
 const path = require("path");
 
+const WebpackProgressBar = require("webpack-progress-bar");
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 /**
  * Create an absolute URL to the given URI inside the current directory
  * @param {string} [uri = ""] - The URI to the desired file
@@ -9,12 +14,27 @@ const path = require("path");
 const here = (uri = "") => path.resolve(__dirname, uri);
 
 module.exports = {
+	stats: "minimal", // For compatibility with friendly-errors-webpack-plugin
 	mode: "production",
 	entry: here("./src/index.ts"),
 	devtool: "source-map",
 	plugins: [
 		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // avoid bundling momentjs locales
+		new WebpackProgressBar(),
+		new FriendlyErrorsPlugin({}),
+		new CompressionWebpackPlugin({
+			test: /\.js$/i,
+			exclude: /node_modules/i,
+		}),
+		// new BundleAnalyzerPlugin({}),
 	],
+	optimization: {
+		usedExports: true,
+		minimize: true,
+		removeEmptyChunks: true,
+		mergeDuplicateChunks: true,
+		concatenateModules: true,
+	},
 	module: {
 		rules: [
 			{
