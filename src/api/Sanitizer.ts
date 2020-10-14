07f5bf232bea0cd03cff973825b10a52a66057e7
Voltra <ludwig.guerin.98@gmx.fr>
2020-10-14 09:41:38 +0200

    Add condition sanitizing API

--- src/api/Sanitizer.ts
+++ src/api/Sanitizer.ts
@@ -2,5 +2,6 @@ import { Optional, ValidationPromise } from "./types";
 import { ValidatorArgs } from "./ValidatorArgs";
 
 export interface Sanitizer<T, U = T> {
+	shouldSanitise(value: T, args: ValidatorArgs): boolean;
 	sanitize(value: T, args: ValidatorArgs): ValidationPromise<U>;
 }
